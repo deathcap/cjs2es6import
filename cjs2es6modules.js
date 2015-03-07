bfcb57f8eff6fae0661c7a4fd2e001f33693c64a
deathcap <deathcap@gmx.co.uk>
2015-03-07 13:57:41 -0800

    Replace node-detective with acorn + walkall

--- cjs2es6modules.js
+++ cjs2es6modules.js
@@ -1,12 +1,37 @@
 'use strict';
 
-var detective = require('./detective');
+var acorn = require('acorn');
+var walk = require('acorn/util/walk');
+var walkall = require('walkall');
+
 var escodegen = require('escodegen');
 var fs = require('fs');
 
 var src = fs.readFileSync(__dirname + '/demo/require-test.js');
-var requires = detective.find(src, {nodes: true});
-console.log(requires);
 
-console.log(escodegen.generate(requires.nodes[0]));
+var parseOpts = {ecmaVersion: 6};
+var ast = acorn.parse(src, parseOpts);
+
+var isRequire = function(node) {
+  var c = node.callee;
+  return c
+    && node.type === 'CallExpression'
+    && c.type === 'Identifier'
+    && c.name === 'require';
+};
+
+walk.simple(ast, walkall.makeVisitors(function(node) {
+  //console.log('Found node type',node.type);
+  if (isRequire(node)) {
+    if (node.arguments.length) {
+      if (node.arguments[0].type === 'Literal') {
+        console.log('Found require:',node.arguments[0].value);
+      } else {
+        console.log('Ignored non-string require:',node.arguments[0]);
+      }
+    }
+  }
+}), walkall.traversers);
+
+//console.log(escodegen.generate(requires.nodes[0]));
 
--- demo/require-test.js
+++ demo/require-test.js
@@ -1,3 +1,5 @@
 var a = require('a');
 var b = require('b');
 var c = require('c');
+var d = require(__dirname + '/d');
+var e = require('e');
