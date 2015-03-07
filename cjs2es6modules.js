6aa791e2bcc562d8fe73826c1b262c17d4c147a3
deathcap <deathcap@gmx.co.uk>
2015-03-07 13:40:59 -0800

    Add copy of node-detective from https://github.com/substack/node-detective

--- cjs2es6modules.js
+++ cjs2es6modules.js
@@ -1,8 +1,12 @@
 'use strict';
 
-var detective = require('detective');
+var detective = require('./detective');
+var escodegen = require('escodegen');
 var fs = require('fs');
 
 var src = fs.readFileSync(__dirname + '/demo/require-test.js');
-var requires = detective(src);
+var requires = detective.find(src, {nodes: true});
 console.log(requires);
+
+console.log(escodegen.generate(requires.nodes[0]));
+
