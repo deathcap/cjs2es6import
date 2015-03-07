480caa81b041ef87e6f5c17ef1cdbaff116193d9
deathcap <deathcap@gmx.co.uk>
2015-03-07 15:59:56 -0800

    Export cjs2es6import function and move demo-specific code

--- cjs2es6import.js
+++ cjs2es6import.js
@@ -3,81 +3,81 @@
 var acorn = require('acorn');
 var walk = require('acorn/util/walk');
 var walkall = require('walkall');
-
 var escodegen = require('escodegen');
-var fs = require('fs');
 
-var src = fs.readFileSync(__dirname + '/demo/require-test.js');
+module.exports = function(src) {
+  var parseOpts = {ecmaVersion: 6};
+  var ast = acorn.parse(src, parseOpts);
 
-var parseOpts = {ecmaVersion: 6};
-var ast = acorn.parse(src, parseOpts);
+  var isRequire = function(node) {
+    var c = node.callee;
+    return c &&
+      node.type === 'CallExpression' &&
+      c.type === 'Identifier' &&
+      c.name === 'require';
+  };
 
-var isRequire = function(node) {
-  var c = node.callee;
-  return c &&
-    node.type === 'CallExpression' &&
-    c.type === 'Identifier' &&
-    c.name === 'require';
-};
+  walk.simple(ast, walkall.makeVisitors(function(anode) {
+    //console.log('Found node type',anode.type,anode);
+    //console.log(escodegen.generate(node));
 
-walk.simple(ast, walkall.makeVisitors(function(anode) {
-  //console.log('Found node type',anode.type,anode);
-  //console.log(escodegen.generate(node));
-
-  if (anode.type !== 'VariableDeclaration') {
-    return;
-  }
-
-  var newNodes = [];
-
-  anode.declarations.forEach(function(node) {
-    if (node.id.type !== 'Identifier') {
-      //console.log('Ignoring non-identifier variable identifier: ',node);
+    if (anode.type !== 'VariableDeclaration') {
       return;
     }
 
-    var varName = node.id.name;
+    var newNodes = [];
 
-    if (isRequire(node.init)) {
-      if (node.init.arguments.length) {
-        if (node.init.arguments[0].type === 'Literal') {
-          var moduleName = node.init.arguments[0].value;
+    anode.declarations.forEach(function(node) {
+      if (node.id.type !== 'Identifier') {
+        //console.log('Ignoring non-identifier variable identifier: ',node);
+        return;
+      }
 
-          //console.log('Found require:', varName, moduleName);
-          //console.log('Old node=',node);
+      var varName = node.id.name;
 
-          delete node.id;
-          delete node.init;
-          node.type =  'ImportDeclaration';
-          node.source = {type: 'Literal', value: moduleName}
+      if (isRequire(node.init)) {
+        if (node.init.arguments.length) {
+          if (node.init.arguments[0].type === 'Literal') {
+            var moduleName = node.init.arguments[0].value;
 
-          node.specifiers = [
-            {
-              type: 'ImportSpecifier',
-              name: null, //{type: 'Identifier', name: varName}, // no 'as', same name as id
-              id: {type: 'Identifier', name: varName}
-            }
-          ];
+            //console.log('Found require:', varName, moduleName);
+            //console.log('Old node=',node);
 
-          newNodes.push(node);
+            delete node.id;
+            delete node.init;
+            node.type =  'ImportDeclaration';
+            node.source = {type: 'Literal', value: moduleName}
 
-          //console.log('New node=',node);
-        } else {
-          //console.log('Ignored non-string require:',node.init.arguments[0]);
-          // Pass it through unchanged, not much we can do TODO: support subset of expression evaluations like browserify
-          newNodes.push(node);
+            node.specifiers = [
+              {
+                type: 'ImportSpecifier',
+                name: null, //{type: 'Identifier', name: varName}, // no 'as', same name as id
+                id: {type: 'Identifier', name: varName}
+              }
+            ];
+
+            newNodes.push(node);
+
+            //console.log('New node=',node);
+          } else {
+            //console.log('Ignored non-string require:',node.init.arguments[0]);
+            // Pass it through unchanged, not much we can do TODO: support subset of expression evaluations like browserify
+            newNodes.push(node);
+          }
         }
       }
-    }
-  });
+    });
 
-  anode.type = 'Program';
-  anode.body = newNodes;
-  delete anode.kind;
-  delete anode.declarations;
-  //console.log('anode=',anode);
+    anode.type = 'Program';
+    anode.body = newNodes;
+    delete anode.kind;
+    delete anode.declarations;
+    //console.log('anode=',anode);
 
-}), walkall.traversers);
+  }), walkall.traversers);
 
-console.log(escodegen.generate(ast));
+  var newSrc = escodegen.generate(ast);
+
+  return newSrc;
+};
 
