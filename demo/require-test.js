fbcbaae405a7e2076f0c138640e0138da6523751
deathcap <deathcap@gmx.co.uk>
2015-03-07 15:15:47 -0800

    require() statements not in variable declarations are not recognized

--- demo/require-test.js
+++ demo/require-test.js
@@ -3,4 +3,7 @@ var b = require('b');
 var c = require('c');
 var d = require(__dirname + '/d');
 var e = require('e');
+require('non-assignment require ignored');
 var foo = require('bar');
+
+//import { hello } from 'test1';
